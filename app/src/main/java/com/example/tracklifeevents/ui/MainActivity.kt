55938ac36551f14b2e61363d2e95795173ba7843
abhic <abhich1607@gmail.com>
2022-09-15 00:01:07 +0530

    Main Activity created

---
+++ app/src/main/java/com/example/tracklifeevents/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tracklifeevents.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tracklifeevents.R
+
+class MainActivity : AppCompatActivity(R.layout.main_activity)
