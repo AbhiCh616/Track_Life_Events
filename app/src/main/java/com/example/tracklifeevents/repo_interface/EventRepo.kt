4704a39a75213a5b74b58670d90dcdc67c46f683
abhic <abhich1607@gmail.com>
2022-09-14 17:30:52 +0530

    getAll method of EventRepo is now returning list as flow

--- app/src/main/java/com/example/tracklifeevents/repo_interface/EventRepo.kt
+++ app/src/main/java/com/example/tracklifeevents/repo_interface/EventRepo.kt
@@ -4,7 +4,7 @@ import com.example.tracklifeevents.valid_model.ValidEvent
 import kotlinx.coroutines.flow.Flow
 
 interface EventRepo {
-    fun getAll(): Flow<ValidEvent>
+    fun getAll(): Flow<List<ValidEvent>>
     suspend fun delete(event: ValidEvent)
     suspend fun insert(event: ValidEvent)
     suspend fun update(event: ValidEvent)
