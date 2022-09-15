3a6317eb4266def8a848567aaed12ecff7455a17
abhic <abhich1607@gmail.com>
2022-09-15 15:07:50 +0530

    Add Video page has event name text field

--- app/src/main/java/com/example/tracklifeevents/ui/add_event/AddEventFragment.kt
+++ app/src/main/java/com/example/tracklifeevents/ui/add_event/AddEventFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.tracklifeevents.R
 import com.example.tracklifeevents.databinding.AddEventBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,5 +24,33 @@ class AddEventFragment : Fragment() {
     ): View {
         binding = AddEventBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.eventNameTextField.let { textField ->
+            textField.setText(viewModel.eventName)
+            textField.doAfterTextChanged { editable ->
+                editable?.let {
+                    viewModel.eventName = editable.toString()
+                }
+            }
+        }
+
+        binding.topAppBar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.save_btn -> {
+                    viewModel.onSaveClick()
+                    findNavController().popBackStack()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
--- app/src/main/java/com/example/tracklifeevents/ui/add_event/AddEventVM.kt
+++ app/src/main/java/com/example/tracklifeevents/ui/add_event/AddEventVM.kt
@@ -1,8 +1,27 @@
 package com.example.tracklifeevents.ui.add_event
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tracklifeevents.model.Event
+import com.example.tracklifeevents.use_case.AddEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AddEventVM @Inject constructor() : ViewModel()
+class AddEventVM @Inject constructor(
+    private val addEvent: AddEvent
+) : ViewModel() {
+    var eventName = ""
+    var eventDate = ""
+    var eventImageUri = ""
+
+    fun onSaveClick() = viewModelScope.launch {
+        val event = Event(
+            name = eventName,
+            date = null,
+            imageUri = eventImageUri
+        )
+        addEvent(event = event)
+    }
+}
