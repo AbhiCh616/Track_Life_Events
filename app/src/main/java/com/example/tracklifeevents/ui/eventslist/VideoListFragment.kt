e0adf4faa38af885eed674e8a0f19b0096ff60e2
abhic <abhich1607@gmail.com>
2022-09-15 12:13:21 +0530

    Event list empty fragment added

---
+++ app/src/main/java/com/example/tracklifeevents/ui/eventslist/VideoListFragment.kt
@@ -0,0 +1,6 @@
+package com.example.tracklifeevents.ui.eventslist
+
+import androidx.fragment.app.Fragment
+import com.example.tracklifeevents.R
+
+class VideoListFragment : Fragment(R.layout.event_list)
